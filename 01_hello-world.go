fb67ef83b32135fe821227f3543b9e679617cea3
minorcell <minorcell6789@gmail.com>
2025-04-09 14:54:07 +0800

    packages and fmt

---
+++ 01_hello-world.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"golearn/utils"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	fmt.Println(utils.GetNow())
+}
+
+// 单行注释
+
+/*
+多行注释
+*/
+
+/*
+1.go run ,go run
+2.go build ,./,go build
+*/
